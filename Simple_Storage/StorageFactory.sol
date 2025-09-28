0ea5a53745eac92f46e417ef892c24d79fe0dc69
Fahadbcd17 <fahadbcd17@gmail.com>
2025-09-28 12:56:38 +0800

    call one contract from another

---
+++ Simple_Storage/StorageFactory.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContract;
+
+    function createSimpleStorageContract() public{
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContract.push(newSimpleStorageContract);
+    }
+
+    function CallStore(uint256 _index, uint256 _address) public{
+        SimpleStorage NewSimpleStorageContract = listOfSimpleStorageContract[_index];
+        NewSimpleStorageContract.store(_address);
+        // listOfSimpleStorageContract[_index].store(_address);
+    }
+
+    function getStore(uint256 _index) public view returns(uint256){
+        SimpleStorage GetSimpleStorageContract = listOfSimpleStorageContract[_index];
+        return GetSimpleStorageContract.retrive();
+        // return listOfSimpleStorageContract[_index].retrive();
+    }
+
+
+}
+
+//     function callStore(uint256 _index, uint256 _simpleStoreAddress) public{
+//         SimpleStorage NewSimpleStore = ListOfSimpleStorage[_index];
+//         NewSimpleStore.store(_simpleStoreAddress);
+//     }
+
+//     function getStore(uint256 _index) public view returns(uint256){
+//         SimpleStorage getSimpleStore = ListOfSimpleStorage[_index];
+//         return getSimpleStore.retrive();
+//     }
+
+// }
